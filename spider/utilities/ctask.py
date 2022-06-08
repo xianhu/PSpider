70b94f913ea02ad1f40ec0e6f71a6d73aa8236e0
qixianhu <qixianhu@qq.com>
2022-06-08 13:53:17 +0800

    update task code

--- spider/utilities/ctask.py
+++ spider/utilities/ctask.py
@@ -12,7 +12,7 @@ class Task(object):
     class of Task, to define task of fetcher, parser and saver
     """
     # class variable, which to parse error message to get a TaskFetch()
-    re_obj = re.compile(r"priority=(?P<p>\d+),\s*keys=(?P<k>.+?),\s*deep=(?P<d>\d+),\s*url=(?P<u>.+)$", flags=re.IGNORECASE)
+    re_obj = re.compile(r"priority=(?P<p>\d+),\s*keys=(?P<k>.+?),\s*deep=(?P<d>\d+),\s*url=(?P<u>.*)$", flags=re.IGNORECASE)
 
     def __init__(self, priority: int = 0, keys: dict = None, deep: int = 0, url: str = None):
         """
@@ -42,7 +42,7 @@ class TaskFetch(Task):
     class of TaskFetch, to define task of fetcher
     """
 
-    def __init__(self, priority: int = 0, keys: dict = None, deep: int = 0, url: str = None, repeat: int = 0):
+    def __init__(self, priority=0, keys=None, deep=0, url=None, repeat: int = 0):
         """
         constructor
         """
@@ -51,17 +51,21 @@ class TaskFetch(Task):
         return
 
     @staticmethod
+    def from_task(task_parse: Task, url: str = None):
+        """
+        initial a TaskFetch() from task_parse and url
+        """
+        priority, keys, deep = task_parse.priority + 1, task_parse.keys, task_parse.deep + 1
+        return TaskFetch(priority=priority, keys=keys, deep=deep, url=url, repeat=0)
+
+    @staticmethod
     def from_string(error_message):
         """
         initial a TaskFetch() from error_message
         """
         reg = Task.re_obj.search(error_message)
-        return TaskFetch(
-            priority=int(reg.group("p")),
-            keys=eval(reg.group("k")),
-            deep=int(reg.group("d")),
-            url=reg.group("u").strip(),
-        )
+        priority, keys, deep, url = [reg.group(i) for i in ["p", "k", "d", "u"]]
+        return TaskFetch(priority=int(priority), keys=eval(keys), deep=int(deep), url=url.strip())
 
 
 class TaskParse(Task):
@@ -69,18 +73,21 @@ class TaskParse(Task):
     class of TaskParse, to define task of parser
     """
 
-    def __init__(
-            self, priority: int = 0, keys: dict = None, deep: int = 0, url: str = None,
-            content: object = None, task_fetch: TaskFetch = None,
-    ):
+    def __init__(self, priority=0, keys=None, deep=0, url=None, content: object = None):
         """
         constructor
         """
-        if task_fetch:
-            priority, keys, deep, url = task_fetch.priority, task_fetch.keys, task_fetch.deep, task_fetch.url
         super().__init__(priority, keys, deep, url)
         self.content = content
         return
+
+    @staticmethod
+    def from_task(task_fetch: Task, content: object = None):
+        """
+        initial a TaskParse() from task_fetch and content
+        """
+        priority, keys, deep, url = task_fetch.priority, task_fetch.keys, task_fetch.deep, task_fetch.url
+        return TaskParse(priority=priority, keys=keys, deep=deep, url=url, content=content)
 
 
 class TaskSave(Task):
@@ -88,15 +95,18 @@ class TaskSave(Task):
     class of TaskSave, to define task of saver
     """
 
-    def __init__(
-            self, priority: int = 0, keys: dict = None, deep: int = 0, url: str = None,
-            item: object = None, task_parse: TaskParse = None,
-    ):
+    def __init__(self, priority=0, keys=None, deep=0, url=None, item: object = None):
         """
         constructor
         """
-        if task_parse:
-            priority, keys, deep, url = task_parse.priority, task_parse.keys, task_parse.deep, task_parse.url
         super().__init__(priority, keys, deep, url)
         self.item = item
         return
+
+    @staticmethod
+    def from_task(task_parse: Task, item: object = None):
+        """
+        initial a TaskSave() from task_parse and item
+        """
+        priority, keys, deep, url = task_parse.priority, task_parse.keys, task_parse.deep, task_parse.url
+        return TaskSave(priority=priority, keys=keys, deep=deep, url=url, item=item)
